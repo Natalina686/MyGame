0a1b98661230b58b6702c48b1d7df2c6a392cff4
Natalina686 <108237928+Natalina686@users.noreply.github.com>
2023-07-18 16:54:53 +0200

    Update main.py

--- main.py
+++ main.py
@@ -8,8 +8,16 @@ WIDTH = 1200
 
 main_display = pygame.display.set_mode((WIDTH, HEIGHT))
 
+player = pygame.Surface((20, 20))
+player.fill((255, 255, 255))
+
 playing = True
+
 while playing:
     for event in pygame.event.get():
         if event.type == QUIT:
             playing = False
+
+            main_display.blit(player, (0, 100))
+
+            pygame.display.flip()
